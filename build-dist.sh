2c361fdd7efe053d5b4dc142e78ed2a4953c6e7f
Lightning323 <sam.p.w524@gmail.com>
2026-09-20 10:23:45 -0600

    Update packs

--- build-dist.sh
+++ build-dist.sh
@@ -3,5 +3,5 @@
 cd pack
 
 packwiz refresh
-# packwiz curseforge export -o ../dist/pack_prebuilt_cf.zip
+packwiz curseforge export -o ../dist/pack_prebuilt_cf.zip
 packwiz modrinth export -o ../dist/pack_prebuilt.mrpack
